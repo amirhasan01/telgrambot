58fd89bb0f93e51b077fb3ff3ccbbfc261cf5831
amirhasan01 <amirhasan1sobhi@gmail.com>
2026-03-25 20:19:03 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
-from telethon import TelegramClient, events
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+from telethon import TelegramClient
 
 api_id = 36979235
 api_hash = "6c17dfff0011cfe631a6f029f5fae3e6"
 
-client = TelegramClient("session_name", api_id, api_hash)
+client = TelegramClient("session", api_id, api_hash)
 
-@client.on(events.NewMessage)
-async def handler(event):
-    if event.raw_text == "hi":
-        await event.reply("Hello!")
+async def main():
+    messages = await client.get_messages("chekhabarre", limit=1)
 
-client.start()
-client.run_until_disconnected()
+    for msg in messages:
+        print("Last message:")
+        print(msg.text)
+
+        # forward message to Saved Messages
+        await client.forward_messages("me", msg)
+
+with client:
+    client.loop.run_until_complete(main())
